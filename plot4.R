24320ae8a49429b735919a7c9f1e007085195021
venkataramanaidu <ramanaidu1956@gmail.com>
2015-10-11 19:43:49 -0400

    Plot 4 files for project 1

--- plot4.R
+++ plot4.R
@@ -1,35 +1,46 @@
-png(file ="plot4.png")
-## Open PNG device; create 'plot.png' in my working directory
-## Create plot and send to a file (no plot appears on screen)
+library(lubridate)
+## Read in full data. Set data and time as character for date/time
+## manipulation later. Remaining variables are set as numeric.
 
-## Read the data and subset
+EPC <- read.table("household_power_consumption.txt", sep=";", header=TRUE , na.strings = "?",
+colClasses = c("character", "character",
+"numeric", "numeric", "numeric",
+"numeric", "numeric", "numeric",
+"numeric"))
 
-EPC <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
-EPCS <- subset(EPC,Date == "1/2/2007" | Date == "1/1/2007" )
+## Insert a new date & time variable for plotting later
+EPC$Date <- dmy(EPC$Date)
+EPC$Time <- hms(EPC$Time)
 
-## coerce the column to numeric
+EPCS <- subset(EPC,Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
 
-EPCS$Sub_metering_1 <- as.numeric(EPCS$Sub_metering_1)
-EPCS$Sub_metering_2 <- as.numeric(EPCS$Sub_metering_2)
-EPCS$Sub_metering_3 <- as.numeric(EPCS$Sub_metering_3)
-EPCS$Voltage <- as.numeric(EPCS$Voltage)
-EPCS$Global_reactive_power <- as.numeric(EPCS$Global_reactive_power)
+EPCS$datetime <- EPCS$Date + EPCS$Time
 
-EPCS$Global_active_power <- as.numeric(EPCS$Global_active_power)
+
+
+## open the device
+
+ png(file ="plot4.png")
+
+## Display 4 graphs 2 by 2
 
 par(mfrow = c(2,2))
+
+
+
 with(EPCS, {
-	plot(EPCS$Global_active_power,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l")
-	plot(EPCS$Voltage,lty = 1,  ylab = "Voltage",type ="l")
-	plot(EPCS$Sub_metering_1,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="black")
-	lines(EPCS$Sub_metering_2,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="red")
-	lines(EPCS$Sub_metering_3,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="blue") 
+	plot(datetime, Global_active_power,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l")
+	plot(datetime, Voltage,lty = 1,  ylab = "Voltage",type ="l")
+	plot(datetime, EPCS$Sub_metering_1,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="black")
+	lines(datetime,EPCS$Sub_metering_2,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="red")
+	lines(datetime, EPCS$Sub_metering_3,lty = 1,  ylab = "Global Active Power(kilowatts)",type ="l",col ="blue") 
 	legend("topright", pch =20,  col = c("black", "blue","red"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-	plot(EPCS$Global_reactive_power,lty = 1,  ylab = "Global_reactive_power",type ="l")
+	plot(datetime,Global_reactive_power,lty = 1,  ylab = "Global_reactive_power",type ="l")
 }
 	
 )
 ## Annotate plot; still nothing on screen
+## Close the device
 dev.off()
 ## Close the PNG file device
 ## Now you can view the file 'pmg4.png' on your computer
