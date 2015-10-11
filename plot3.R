7967bcc07d794a07b8b62f78b9539088d850ddad
venkataramanaidu <ramanaidu1956@gmail.com>
2015-10-11 18:20:06 -0400

    Plot 3 Files for Project 1

--- plot3.R
+++ plot3.R
@@ -1,31 +1,39 @@
-png(file ="plot3.png")
-## Open PNG device; create 'plot.png' in my working directory
-## Create plot and send to a file (no plot appears on screen)
+library(lubridate)
+## Read in full data. Set data and time as character for date/time
+## manipulation later. Remaining variables are set as numeric.
+
 
 ## Read the data and subset
 
-EPC <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
-EPCS <- subset(EPC,Date == "1/2/2007" | Date == "1/1/2007" )
+EPC <- read.table("household_power_consumption.txt", sep=";", header=TRUE , na.strings = "?",
+colClasses = c("character", "character",
+"numeric", "numeric", "numeric",
+"numeric", "numeric", "numeric",
+"numeric"))
 
-## coerce the column to numeric
+## Insert a new date & time variable for plotting later
+EPC$Date <- dmy(EPC$Date)
+EPC$Time <- hms(EPC$Time)
 
-EPCS$Sub_metering_1 <- as.numeric(EPCS$Sub_metering_1)
-EPCS$Sub_metering_2 <- as.numeric(EPCS$Sub_metering_2)
-EPCS$Sub_metering_3 <- as.numeric(EPCS$Sub_metering_3)
+EPCS <- subset(EPC,Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
 
+EPCS$datetime <- EPCS$Date + EPCS$Time
 
-
-{
-plot(EPCS$Sub_metering_1,lty = 1,  ylab = "Energy Sub Metering",type ="l",col ="black")
-lines(EPCS$Sub_metering_2,lty = 1,  ylab = "Energy Sub Metering",type ="l",col ="red")
-lines(EPCS$Sub_metering_3,lty = 1,  ylab = "Energy Sub Metering",type ="l",col ="blue") 
-legend("topright", pch =20,  col = c("black", "blue","red"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-}
-
-
-
-
-## Annotate plot; still nothing on screen
+png(file ="plot3.png")
+## Open PNG device; create 'plot.png' in my working directory
+## Create plot and send to a file (no plot appears on screen)
+## Plot in the order 1) submeter1, 2) submeter2, 3) submeter3 as specified
+## in project instructions
+with(EPCS, plot(datetime, Sub_metering_1, type = "l", xlab = "",
+ylab = "Energy sub metering"))
+with(EPCS, lines(datetime, Sub_metering_2, col = "red"))
+with(EPCS, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+col = c("black", "red", "blue"), lty = "solid")
+## Close graphics device
 dev.off()
-## Close the PNG file device
-## Now you can view the file 'myplot.pdf' on your computer
+
+
+
+
+
